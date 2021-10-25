f0c88349ef39d6efcc1cb13ccf3ebb0ff13e6591
Иванов Александр Алексеевич <aleksand7@yandex.ru>
2021-10-25 12:12:24 +0300

    source

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build Your Own React октябрь 2021
+
+[Статья](https://pomb.us/build-your-own-react/)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"],
+  },
+	plugins: [new HtmlWebpackPlugin({
+		template: "./src/index.html"
+	})],
+};
